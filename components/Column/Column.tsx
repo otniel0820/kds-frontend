9003bc2b52b1f117e3dd90069192030a576c7b93
Otniel Lascano <otniel.lascano@ikualo.com>
2026-02-11 02:34:02 +0100

    Refactor Column component to improve structure and styling, updating props and layout for better usability

--- components/Column/Column.tsx
+++ components/Column/Column.tsx
@@ -1,35 +1,40 @@
-import s from "./Column.module.scss"
-import { Order } from "@/dtos/Order.dto"
+import { memo } from "react";
+import { useDroppable } from "@dnd-kit/core";
+import clsx from "classnames";
+import { OrderStatus } from "@/domain/order/order-status";
+import s from "./Column.module.scss";
 
-export type ColumnProps = {
-	orders: Array<Order>
-	title: string
-	onClick?: (order: Order) => void
+type ColumnProps = {
+  status: OrderStatus;
+  title: string;
+  count: number;
+  children: React.ReactNode;
+};
+
+function Column({ status, title, count, children }: ColumnProps) {
+  const { setNodeRef, isOver } = useDroppable({
+    id: status,
+  });
+
+  return (
+    <div className={s["pk-column"]}>
+      <div className={s["pk-column__title"]}>
+        <h3>
+          {title} ({count})
+        </h3>
+      </div>
+
+      <div
+        ref={setNodeRef}
+        className={clsx(
+          s["pk-column__content"],
+          isOver && s["pk-column__content--over"]
+        )}
+      >
+        {children}
+      </div>
+    </div>
+  );
 }
 
-export default function Column(props: ColumnProps) {
-	return (
-		<div className={s["pk-column"]}>
-			<div className={s["pk-column__title"]}>
-				<h3>{props.title}</h3>
-			</div>
-			{props.orders.map((order) => (
-				<div
-					onClick={() => props.onClick && props.onClick(order)}
-					className={s["pk-card"]}
-				>
-					<div>
-						<span>
-							Orden: <b>{order.id}</b>
-						</span>
-					</div>
-					<div>
-						{order.items.map((item) => (
-							<div></div>
-						))}
-					</div>
-				</div>
-			))}
-		</div>
-	)
-}
+export default memo(Column);
