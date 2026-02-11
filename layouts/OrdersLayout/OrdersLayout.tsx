1301a2d9e500e246a6111080faea81ff45b2de95
Otniel Lascano <otniel.lascano@ikualo.com>
2026-02-11 02:35:53 +0100

    Refactor OrdersLayout styles and structure for improved layout and responsiveness

--- layouts/OrdersLayout/OrdersLayout.tsx
+++ layouts/OrdersLayout/OrdersLayout.tsx
@@ -1,19 +1,32 @@
-import Logo from "@/bases/Logo/Logo"
-import s from "./OrdersLayout.module.scss"
-import Riders from "@/components/Riders/Riders"
-import Kanban from "@/components/Kanban/Kanban"
+import Logo from "@/bases/Logo/Logo";
+import s from "./OrdersLayout.module.scss";
+import Kanban from "@/components/Kanban/Kanban";
+import ThemeSwitch from "@/components/theme/ThemeSwitch";
 
 export default function OrdersLayout() {
-	return (
-		<main className={s["pk-layout"]}>
-		<nav className={s["pk-layout__navbar"]}>
-			<Logo size="M" />
-			<strong>KDS: Krazy Display Service</strong>
-		</nav>
-			<article className={s["pk-layout__app"]}>
-				<Kanban />
-				<Riders />
-			</article>
-		</main>
-	)
+  return (
+    <main className={s["pk-layout"]}>
+      <nav className={s["pk-layout__navbar"]}>
+        <div className={s["pk-layout__navbar-left"]}>
+          <Logo size="M" />
+          <div className={s["pk-layout__brand"]}>
+            <span className={s["pk-layout__title"]}>
+              KDS: Krazy Display Service
+            </span>
+            <span className={s["pk-layout__subtitle"]}>
+              Kitchen Dashboard System
+            </span>
+          </div>
+        </div>
+
+        <div className={s["pk-layout__navbar-right"]}>
+          <ThemeSwitch />
+        </div>
+      </nav>
+
+      <article className={s["pk-layout__app"]}>
+        <Kanban />
+      </article>
+    </main>
+  );
 }
