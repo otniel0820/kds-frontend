d41f2b7c7c91ad7aa9bef17eb4b4c3a82f6058ab
Otniel Lascano <otniel.lascano@ikualo.com>
2026-02-11 02:34:38 +0100

    Refactor Riders component to use Card for individual rider display and enhance styling

--- components/Riders/Riders.tsx
+++ components/Riders/Riders.tsx
@@ -1,17 +1,45 @@
-import s from "./Riders.module.scss"
-import Rider from "@/bases/Rider/Rider"
-import { useRiders } from "@/contexts/Riders.context"
+import { memo } from "react";
+import clsx from "classnames";
 
-export default function Riders() {
-	const { riders } = useRiders()
-	return (
-		<section className={s["pk-riders__container"]}>
-			<div className={s["pk-riders"]}>
-				<h3>Repartidores:</h3>
-				{riders.map((rider) => (
-					<Rider {...rider} />
-				))}
-			</div>
-		</section>
-	)
+import RiderAvatar from "@/bases/Rider/Rider";
+import Card from "@/bases/card/Card";
+
+import type { RiderDto } from "@/dtos/Rider.dto";
+import s from "./Riders.module.scss";
+
+type RidersProps = {
+  rider: RiderDto;
+  onClick?: () => void;
+};
+
+function Riders({ rider, onClick }: RidersProps) {
+  return (
+    <Card
+      size="md"
+      variant="elevated"
+      className={clsx(s.riderCard)}
+      onClick={(e) => {
+        e.stopPropagation();
+        onClick?.();
+      }}
+    >
+      <div className={s.content}>
+        <div className={s.info}>
+          <div className={s.partner}>{rider.partnerName}</div>
+
+          <div className={s.orderNumber}>Orden: {rider.displayNumber}</div>
+
+          <div className={s.name}>{rider.name}</div>
+
+          <div className={s.number}>Rider #{rider.riderNumber}</div>
+        </div>
+
+        <div className={s.avatar}>
+          <RiderAvatar />
+        </div>
+      </div>
+    </Card>
+  );
 }
+
+export default memo(Riders);
